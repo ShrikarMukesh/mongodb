d2e7d714c4e84f9d3f332bd7b5c15135bc00daa3
Shrikar Mukesh <mukesh.shrikar7@gmail.com>
2025-01-11 22:55:03 +0530

    Practiced Mongod Aggregation function

---
+++ queries-practice/aggree1.mongodb.js
@@ -0,0 +1,12 @@
+
+use('aggregation');
+
+
+const database = 'usermanagement';
+const collection = 'authors';
+
+// Create a new database.
+use(database);
+
+// Create a new collection.
+db.createCollection(collection);
